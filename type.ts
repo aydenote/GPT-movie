371094385568dbc2cb8e8227b9b548af7716b5cb
aydenote <dslgpgh@gmail.com>
2023-03-30 16:48:33 +0900

    리덕스 상태 관련 공통 타입 정의

--- type.ts
+++ type.ts
@@ -1,4 +1,4 @@
-export interface Movie {
+export type Movie = {
   adult: boolean;
   backdrop_path: string;
   genre_ids: number[];
@@ -14,4 +14,12 @@ export interface Movie {
   video: boolean;
   vote_average: number;
   vote_count: number;
-}
+};
+
+export type ReduxType = {
+  currentId: number;
+  dataList: {
+    question: string;
+    answer: string;
+  }[];
+};
