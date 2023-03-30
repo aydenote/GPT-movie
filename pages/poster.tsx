7db0836528f10344927a5cc39e4355aec2df6b8d
aydenote <dslgpgh@gmail.com>
2023-03-30 16:49:18 +0900

    타입 파일명 변경으로 경로 수정

--- pages/poster.tsx
+++ pages/poster.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Movie } from '@/type/movie';
+import { Movie } from '@/type';
 import LoadingSpinner from '@/components/LoadingSpinner ';
 import Modal from '@/components/modal';
 import { getTrendingMovie } from './api/movie';
