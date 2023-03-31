1526901789b89ff263d0cd8e338540693ca9aaff
aydenote <dslgpgh@gmail.com>
2023-03-31 22:40:06 +0900

    영화 추천 OpenAI API 작성

---
+++ pages/api/generate.ts
@@ -0,0 +1,28 @@
+import { Configuration, OpenAIApi } from 'openai';
+import type { Request, Response } from 'express';
+
+const configuration = new Configuration({
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req: Request, res: Response) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: 'OpenAI API key not configured',
+      },
+    });
+    return;
+  }
+
+  const question = req.body.question || '';
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: question,
+    temperature: 0,
+    max_tokens: 10,
+  });
+
+  res.status(200).json({ result: response.data.choices[0].text });
+}
