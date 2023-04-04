1bf999da97d5d6d1a39f50d97b0d9a3b3429bfed
aydenote <dslgpgh@gmail.com>
2023-04-04 20:47:22 +0900

    일일 트렌드 영화 데이터 20개에서 4개로 변경.

--- pages/api/movie.ts
+++ pages/api/movie.ts
@@ -13,17 +13,18 @@ export async function getWeekTrend() {
 /** 영화 제목으로 정보 검색 */
 export async function getSearchMovie(title: string) {
   const search = await axios.get(
-    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.NEXT_PUBLIC_TMDB_KEY}&page=1&query=${title}&language=ko-KR`
+    `https://api.themoviedb.org/3/search/movie?language=ko-KR&api_key=${process.env.NEXT_PUBLIC_TMDB_KEY}&page=1&query=${title}&language=ko-KR`
   );
   return search.data.results[0];
 }
 
-/** 오늘의 트렌드 영화 추천 */
+/** 오늘의 트렌드 영화 정보 가져오기 */
 export async function getTodayTrend() {
   const movieListReq = await axios.get(
-    `https://api.themoviedb.org/3/trending/movie/day?&api_key=${process.env.NEXT_PUBLIC_TMDB_KEY}`
+    `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.NEXT_PUBLIC_TMDB_KEY}&language=ko-KR&per_page=5&page=1`
   );
-  const movieList = await movieListReq.data.results;
+
+  const movieList = await movieListReq.data.results.slice(0, 4);
 
   return movieList;
 }
