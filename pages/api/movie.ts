985366afdda274a655731626930ad346a3f29c8f
aydenote <dslgpgh@gmail.com>
2023-04-06 16:14:21 +0900

    금일 추천 영화 정보 2개로 수정

--- pages/api/movie.ts
+++ pages/api/movie.ts
@@ -21,10 +21,10 @@ export async function getSearchMovie(title: string) {
 /** 오늘의 트렌드 영화 정보 가져오기 */
 export async function getTodayTrend() {
   const movieListReq = await axios.get(
-    `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.NEXT_PUBLIC_TMDB_KEY}&language=ko-KR&per_page=5&page=1`
+    `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.NEXT_PUBLIC_TMDB_KEY}&page=1&language=ko-KR`
   );
 
-  const movieList = await movieListReq.data.results.slice(0, 4);
+  const movieList = await movieListReq.data.results.slice(0, 2);
 
   return movieList;
 }
